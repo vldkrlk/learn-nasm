f232ee07aa09ffad1fd827b0a9a462add71ff21a
VladKoroliuk <koroliuk.work@gmail.com>
2025-01-11 22:36:43 +0200

    added comments

--- euclid/euclid.asm
+++ euclid/euclid.asm
@@ -1,19 +1,25 @@
 %include "../include/stud_io.inc"
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;; Euclid's algorithm
+;;;;;
+;;;;; This implementation was created for educational
+;;;;; purposes and supports 8-bit numbers as input
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 global _start
 section .data
-    a db 97
+    a db 97          ; input data 
     b db 89
 section .text
 _start:
-    mov cl, byte [a]
+    mov cl, byte [a] ; use registers to access input data
     mov bl, byte [b]
-again:
-    xor ah, ah
-    mov al, cl
-    div bl
-    mov cl, bl
-    mov bl, ah
-    cmp ah, 0
-    jne again
-    FINISH
+again:               ; cycle start
+    xor ah, ah       ; make remainder 0 before dividing
+    mov al, cl       ; prepare number for division
+    div bl           ; divide by second number
+    mov cl, bl       ; set divisible for next iteration 
+    mov bl, ah       ; set remainder as divisor for next iteration
+    cmp ah, 0        ; compare remainder with 0
+    jne again        ; if not, jump to next iteration
+    FINISH           ; the result is stored in the CL register
