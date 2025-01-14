84a091dec33c2f3e60e46c3533b8d27b15f7f296
VladKoroliuk <koroliuk.work@gmail.com>
2025-01-14 15:34:31 +0200

    factorial

--- factorial/factorial.asm
+++ factorial/factorial.asm
@@ -6,17 +6,14 @@
 global _start
 section .data
     input  dd 5         ; input 
-    result dq ?         ; result   
 section .text
 _start:
-    mov ebx, [input]    ; previous number
-    mov ecx, 1          ; here's the result
-again:                  
-    cmp ebx, 1          ; 
-    je finish
+    mov eax, 1          
+    mov ebx, [input]
+lp: cmp ebx, 1
+    jng finish
     mul ebx
     dec ebx
-    jmp again
-
-finish:
+    jmp lp
+finish:                 ; result is in eax
     FINISH
